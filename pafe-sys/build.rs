d0808d0a3aa9503b26aeeb8ab9a23514e8142e4a
near129 <ryou.12912@gmail.com>
2024-06-21 18:09:49 +0900

    fix: build.rs

--- pafe-sys/build.rs
+++ pafe-sys/build.rs
@@ -2,13 +2,19 @@ use std::env;
 use std::path::PathBuf;
 
 #[cfg(feature = "vendored")]
-fn build(libdir_path: &PathBuf, out_dir: &str) {
-    let tmp_libdir_path = PathBuf::from(out_dir).join("libpafe");
+fn build(libdir_path: &PathBuf, out_dir: &PathBuf) {
+    let tmp_libdir_path = out_dir.join("libpafe");
+    if tmp_libdir_path.exists() {
+        std::fs::remove_dir_all(&tmp_libdir_path).unwrap();
+    }
     copy_dir::copy_dir(libdir_path.clone(), tmp_libdir_path.clone()).unwrap();
+    // It may not work well when cross-compiling, so there is a possibility of modification.
     if !std::process::Command::new(tmp_libdir_path.join("configure"))
         .arg("--build")
         .arg(env::var("HOST").unwrap())
         .arg("--host")
+        .arg(env::var("HOST").unwrap())
+        .arg("--target")
         .arg(env::var("TARGET").unwrap())
         .arg("--prefix")
         .arg(&out_dir)
@@ -39,20 +45,22 @@ fn build(libdir_path: &PathBuf, out_dir: &str) {
 }
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap())
+        .canonicalize()
+        .expect("cannot canonicalize path");
     let libdir_path = PathBuf::from("libpafe")
         .canonicalize()
         .expect("cannot canonicalize path");
     let headers_path = libdir_path.join("src/libpafe.h");
 
-    println!("cargo:rustc-link-lib=libpafe");
-    println!("cargo:rustc-link-lib=dylib=usb-1.0");
-
     #[cfg(feature = "vendored")]
     {
         build(&libdir_path, &out_dir);
-        println!("cargo:rustc-link-search={}", out_dir);
+        println!("cargo:rustc-link-search={}", out_dir.join("lib").display());
     }
+
+    println!("cargo:rustc-link-lib=pafe");
+    println!("cargo:rustc-link-lib=dylib=usb-1.0");
 
     let bindings = bindgen::Builder::default()
         .header(headers_path.to_str().unwrap())
